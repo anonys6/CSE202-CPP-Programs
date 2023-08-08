b22b38b4e7bb7c2c3d8f7ff7753559bc793b8790
anonys6 <shakibalam998@gmail.com>
2023-08-08 22:22:26 +0530

    initial commit

---
+++ 1_shift_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
+    }
+
+    int rem;
+    cout << "\nWhat you want to remove? "<<endl;
+    cin>>rem;
+
+    for (int i = rem; i < arrSize; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+
+    cout << "\nAfter shifting: \n";
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        cout << arr[i] << " ";
+    }
+
+}
