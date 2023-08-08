92cf752899452e3cac67dbac9486c7eceb97f5c8
anonys6 <shakibalam998@gmail.com>
2023-08-08 22:29:14 +0530

    week 1 question 1 and 2 of assignments

--- NPTEL/week1.cpp
+++ NPTEL/week1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+
+using namespace std;
 
 int main () {
-    int n = 4;
+    // int n = 4;
     // std :: cout << std::pow(n, 2) << " " << std::sqrt(n);
+    // cout << pow(n, 2) << " " << sqrt(n);
+    
+    // vector<int> arr;
+    // vector <int> arr(3);
+    // vector <int> arr[3];
+    // int arr[3];
+    
+    arr.resize(10);
+    for (int i = 0; i < 10; i++)
+        arr[i] = i + 1;
     return 0;
 }
