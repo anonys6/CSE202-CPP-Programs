eae30785ee5a59e3fcc59d3ffdb1e079d83513a1
anonys6 <shakibalam998@gmail.com>
2023-08-09 18:02:52 +0530

    naresh sir class

---
+++ 2_check_triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a, b, c;
+    cout<<"Enter angles of triangle: ";
+    cin >> a >> b >> c;
+    if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+        cout<< "Right Angle";
+    else
+        cout <<"Not Right Angle";
+    return 0;
+}
---
+++ 3_pascal_triangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    
+    
+    2 aug tak done
+}
