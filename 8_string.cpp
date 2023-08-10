05296c473e47d040add2cf30776596ccd6af887e
anonys6 <shakibalam998@gmail.com>
2023-08-10 19:43:54 +0530

    string

--- 8_string.cpp
+++ 8_string.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main()
 {
-    string s = "abc";
-    cout<<s.length();
+    string s = "abcdef";
     
-    // cout << arr;
-
+    cout<<s.substr(2);
+    
     return 0;
 }
