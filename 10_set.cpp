bb5f83fd4189ab9d8901732928c9418b753327da
anonys6 <shakibalam998@gmail.com>
2023-08-16 19:17:01 +0530

    set header files

---
+++ 10_set.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set <int> s;
+    
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(1);
+
+    for (auto i : s) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
