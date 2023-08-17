6cbdd7b8794076fb10558a6727e76628afb54d3a
anonys6 <shakibalam998@gmail.com>
2023-08-17 21:02:01 +0530

    class, structure, union and enum

---
+++ 12_class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Test
+{
+    string name;
+
+public:
+    Test(string name)
+    {
+        this->name = name;
+    }
+
+    ~Test()
+    {
+        cout << this->name << endl;
+    }
+};
+
+int main()
+{
+    Test t1("A");
+    Test t2("C");
+    Test t3("B");
+    return 0;
+}
