dcdd35b402f3308ff6f84578f642aa4b67ef3f1d
anonys6 <shakibalam998@gmail.com>
2023-10-02 12:39:09 +0530

    hello in new style

---
+++ 38_file_handling.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    fstream myFile;
+
+    // // write in file
+    // myFile.open("38_file.txt", ios::out);
+    // if (myFile.is_open()) {
+    //     myFile << "Hello" << endl;
+    //     myFile << "This is second line" << endl;
+    //     myFile.close();
+    // }
+
+    // // append a file
+    // myFile.open ("38_file.txt", ios::app);
+    // if (myFile.is_open()) {
+    //     myFile << "Hello2" << endl;
+    //     myFile.close();
+    // }
+
+    // read a file
+    myFile.open("38_file.txt", ios::in);
+    if (myFile.is_open()) {
+        string line;
+        while (getline(myFile, line)) {
+            fstream myFile2;
+            myFile2.open("38_file2.txt", ios::app);
+            if (myFile2.is_open()) {
+                myFile2 << line << endl;
+                myFile2.close();
+            }
+            cout << line << endl;
+        }
+    }
+    
+    return 0;
+}
--- check.cpp
+++ check.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
+
 using namespace std;
-
-class course
-{
-    int x, y;
-
-public:
-    void Initialize(int xx)
-    {
-        x = ++xx;
-    }
-
-    void Display()
-    {
-        cout << --x << " ";
-    }
-};
 
 int main()
 {
-    course obj;
-    obj.Initialize(20);
-    obj.Display();
-    int* p = (int*)&obj;
-    *p = 5;
-    obj.Display();
+    string text = "hello world";
+
+    char ans[12];
+
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') {
+            ans[i] = ' ';
+            continue;
+        }
+        char ch = 'a';
+        if (ans == text) {
+            break;
+        }
+        for (int j = 0; j < 26; j++) {
+            ans[i] = ch;
+            if (ans[i] == text[i]) {
+                cout << ans << endl;
+                break;
+            } else {
+                cout << ans << endl;
+            }
+            ch++;
+        }
+    }
+
+    cout << ans << endl;
+    
     return 0;
 }
