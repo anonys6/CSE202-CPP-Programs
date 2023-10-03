6a12a94506e8e9f0ee2c52a8e08c1fd50857ff42
anonys6 <shakibalam998@gmail.com>
2023-10-03 19:11:13 +0530

    files in cpp

---
+++ 39_hello_for.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text = "hello world";
+
+    char ans[12];
+
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') {
+            ans[i] = ' ';
+            continue;
+        }
+        char ch = 'a';
+        if (ans == text) {
+            break;
+        }
+        for (int j = 0; j < 26; j++) {
+            if (!isalpha(ch)) {
+                continue;
+            } else {
+                ans[i] = ch;
+            }
+            if (ans[i] == text[i]) {
+                cout << ans << endl;
+                break;
+            } else {
+                cout << ans << endl;
+            }
+            ch++;
+        }
+    }
+
+    cout << ans << endl;
+    
+    return 0;
+}
