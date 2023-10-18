a96ece1e8c21cfd164472f67d95fcc9efa3a7c99
anonys6 <shakibalam998@gmail.com>
2023-10-18 19:42:01 +0530

    appending in a file

---
+++ LPU_Colab/CPP/Lec_18_q2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    fstream myFile;
+
+    string str;
+    getline(cin, str);
+    
+    myFile.open("string.txt", ios::app);
+
+    if(!myFile.is_open()) {
+        cout << "Cannot open file" << endl;
+        return 1;
+    } else {
+        myFile << str << endl;
+    }
+
+    cout << "Data appended successfully" << endl;
+
+    return 0;
+}
