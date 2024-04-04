d245ad822bd47c18d4be84ec1f6c0ae9104d477c
anonys6 <shakibalam998@gmail.com>
2024-04-04 20:19:06 +0530

    checking new changes

--- check.cpp
+++ check.cpp
@@ -1,54 +1,9 @@
-#include "stdio.h"
+#include <iostream>
+#include <cmath>
 
-long int fact(long int n);
+
+using namespace std;
 
 int main() {
-    long int n;
-    scanf("%ld", &n);
-
-    if (n == 0) {
-        printf("%ld", 1);
-        return 0;
-    }
-
-    printf("%ld \n", fact(n));
-    return 0;
+    cout<<"Hello" << endl;;
 }
-
-long int fact(long int n) {
-    if (n >= 1)
-        return n * fact(n - 1);
-    else
-        return 1;
-}
-
-// #include <stdio.h>
-
-// struct Student {
-//     char name[50];
-//     int roll;
-//     float marks;
-// };
-
-// int main() {
-//     struct Student s;
-
-//     printf("Enter information of students:\n");
-
-//     printf("Enter name: ");
-//     scanf("%s", s.name);
-
-//     printf("Enter roll number: ");
-//     scanf("%d", &s.roll);
-
-//     printf("Enter marks: ");
-//     scanf("%f", &s.marks);
-
-//     // Print student details
-//     printf("Displaying Information:\n");
-//     printf("Name: %s\n", s.name);
-//     printf("Roll: %d\n", s.roll);
-//     printf("Marks: %.2f\n", s.marks);
-
-//     return 0;
-// }
